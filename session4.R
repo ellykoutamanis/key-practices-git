e769299f7ba599f5e9523ee02bebef7b3151d66d
ellykoutamanis <e.koutamanis@let.ru.nl>
2021-04-16 20:34:51 +0200

    Added script header, change log, sections. Deleted unnecessary comments.  Changed variable names into more meaningful names.

--- session4.R
+++ session4.R
@@ -1,19 +1,45 @@
-# 1. Set up your project by loading your packages, e.g.:
+#----Script Header------------------------------------------------------####
+#Date:        16.04.2021
+#Author:      Elly Koutamanis
+#Filename:    session4.R
+#Description: Example code created by Julia Egger and 
+#             updated by Elly Koutamanis as homework for the session
+#             "Best practices for programming in R" by Arushi Garg.
+#             The script does a simple anova analysis.
+#Project:     Key Practices for the Language Scientist 2021
+#-----------------------------------------------------------------------###
+
+#----Change log----####
+#Date:        16.04.2021
+#Change by:   Elly Koutamanis
+#Changes:     Script header added
+#             Sections added
+#             Comments describing *what* is happening deleted
+#             Comments describing *why* things are done added
+#             Variable names changed into more meaningful and consistent names
+#Purpose:     Increase readability
+
+
+#----Preliminaries----####
 library(package)
-
-# 2. Set the working directory and load the data, e.g.:
 setwd("directory")
-data <- read.delim("data.csv", header = TRUE, sep = ",")
 
-# 3. Make variables ready to work on, e.g.:
-data <- data %>%
+
+#----Data preparation----####
+AllData <- read.delim("data.csv", header = TRUE, sep = ",")
+
+RTDataInclNA <- AllData %>%
             select(ppID, condition, trial, RT) %>%
             mutate(condition = as.factor(condition), target = as.factor(target))
-data <- na.omit(data)
 
-# 4. Explore data, e.g.:
-summary(data)
+#Only complete cases can be included in final analysis:
+RTData <- na.omit(RTDataInclNA)
 
-# 5. Analysis (can be a simple analysis), e.g. a one-way ANOVA, within-subjects:
-anovaRTbycategory <- aov(RT ~category+error(ppID/category), data = analysis_anova)
-summary(anovaRTbycategory)
+
+#----Data exploration----####
+summary(RTData)
+
+
+#----Analysis: one-way Anova----####
+AnovaRTByCategory <- aov(RT ~category+error(ppID/category), data = analysis_anova)
+summary(AnovaRTByCategory)
